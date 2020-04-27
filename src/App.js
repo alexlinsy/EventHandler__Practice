2191c5ddeef48876858b2d34a97ea41a6826804f
alexlinsy <39345015+alexlinsy@users.noreply.github.com>
2020-04-27 16:49:08 -0300

    Practice eventHandler with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [events, setEvents] = useState({ click: 0, name: "" });
+  function handleClicks() {
+    setEvents({ ...events, click: events.click + 1 });
+  }
+  function handleChange(event) {
+    setEvents({ ...events, name: event.target.value });
+  }
+  function resetClicks() {
+    setEvents({ ...events, click: 0 });
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p>There have been {events.click} clicks</p>
+      <button onClick={handleClicks}>Click</button>
+      <button onClick={resetClicks}>Reset</button>
+      <p>Your name is: {events.name}</p>
+      <input onChange={handleChange} />
+      {events.name ? <p>Hello {events.name}</p> : null}
     </div>
   );
 }
