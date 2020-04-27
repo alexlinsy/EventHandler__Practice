9494c70d6ae5640680299902bad8655368cd10ad
alexlinsy <39345015+alexlinsy@users.noreply.github.com>
2020-04-27 16:53:48 -0300

    Practice useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [events, setEvents] = useState({ click: 0, name: "" });
+  const [events, setEvents] = useState({
+    click: 0,
+    name: window.localStorage.getItem("name") || ""
+  });
   function handleClicks() {
     setEvents({ ...events, click: events.click + 1 });
   }
@@ -12,13 +15,16 @@ export default function App() {
   function resetClicks() {
     setEvents({ ...events, click: 0 });
   }
+  useEffect(() => {
+    window.localStorage.setItem("name", events.name);
+  });
   return (
     <div className="App">
       <p>There have been {events.click} clicks</p>
       <button onClick={handleClicks}>Click</button>
       <button onClick={resetClicks}>Reset</button>
       <p>Your name is: {events.name}</p>
-      <input onChange={handleChange} />
+      <input value={events.name} onChange={handleChange} />
       {events.name ? <p>Hello {events.name}</p> : null}
     </div>
   );
